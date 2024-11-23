b7b07e252804b6925ad2a5a2c2cf1b8daf48203a
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2024-11-23 10:04:18 +0700

    develop footballer api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const route = require('./routes/authRoute');
+const footballerroute = require('./routes/footballerRoute');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use("/api/auth", route);
+app.use("/api", footballerroute);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
