30a420607ffb9c506bb57bd98cf9d8dd69d099cf
vouchh <vouchhean.mom@institute.pse.ngo>
2024-11-25 14:02:30 +0700

    updated

--- model/authModel.js
+++ model/authModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       required: true,
-      enum: ["admin", "user"],
+      enum: ["admin", "user"],default: "user"
     },
   },
   {
